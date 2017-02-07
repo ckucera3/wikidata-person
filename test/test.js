41328ca007f3cb5a00c7d9dbd52ff73c2eceb7f6
ckucera3 <claytonkucera@mgail.com>
2017-02-07 01:15:28 -0500

    add tests; update gitignore;

--- test/test.js
+++ test/test.js
@@ -3,36 +3,73 @@
 var expect = require('chai').expect;
 var wdp = require('../src/wikidataPerson');
 
-describe('#wdp', function() {
-    it('should retrieve JSON of Emperor Charles V', function() {
-        var testPromise = new Promise(function(resolve, reject) {
-            wdp('Emperor Charles V', function(result) {
-                resolve(result);
+describe('wdp', function() {
+
+    describe('#personQuery', function() {
+        it('should retrieve JSON of Emperor Charles V', function() {
+            var testPromise = new Promise(function(resolve, reject) {
+                wdp.person('Emperor Charles V', function(result) {
+                    resolve(result);
+                });
+            });
+
+            return testPromise.then(function(result){
+
+                expect(JSON.stringify(result)).to.equal(
+                    JSON.stringify({
+                        "name": "Charles V",
+                        "description": "Holy Roman Emperor",
+                        "birthdate": "+1500",
+                        "deathdate": "+1558"
+                    }));
             });
         });
 
-        return testPromise.then(function(result){
+        it('should return undefined', function() {
+            var testPromise = new Promise(function(resolve, reject) {
+                wdp.person('123', function(result) {
+                    resolve(result);
+                });
+            });
 
-            expect(JSON.stringify(result)).to.equal(
-            JSON.stringify({
-                "name": "Charles V",
-                "description": "Holy Roman Emperor",
-                "birthdate": new Date("1500-12-31T23:59:59.999Z"),
-                "deathdate": new Date("1558-12-31T23:59:59.999Z")
-            }));
+            return testPromise.then(function(result){
+                expect(result).to.equal(undefined);
+            });
         });
     });
 
-    it('should return undefined', function() {
-        var testPromise = new Promise(function(resolve, reject) {
-            wdp('123', function(result) {
-                resolve(result);
+    describe('#categoryQuery', function() {
+        it('should retrieve the entities belonging to the `Category:2nd-century_Roman_usurpers`', function() {
+            this.timeout(0);
+            var testPromise = new Promise(function(resolve, reject) {
+                wdp.category('2nd-century_Roman_usurpers', function(result) {
+                    resolve(result);
+                });
+            });
+
+            return testPromise.then(function(result){
+
+                expect(JSON.stringify(result)).to.equal(
+                    JSON.stringify(
+                        [{
+                            name: 'Avidius Cassius',
+                            description: 'Roman consul',
+                            birthdate: '+0130',
+                            deathdate: '+0175'
+                        },
+                        {
+                            name: 'Clodius Albinus',
+                            description: 'Roman usurper proclaimed emperor by the legions in Britain and Hispania',
+                            birthdate: '+0150',
+                            deathdate: '+0197'
+                        }
+                        ]
+                    ));
             });
         });
 
-        return testPromise.then(function(result){
-            expect(result).to.equal(undefined);
-        });
-    });
+    })
+
+
 
 });
